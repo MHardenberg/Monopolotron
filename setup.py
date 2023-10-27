d15e17136be2f1d91003776166679db146d42d61
Michel Hardenberg <MHardenberg@proton.me>
2023-10-27 13:08:10 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+   name='Monopolotron',
+   version='0.1',
+   packages=['pipenv',],
+   license='BSD 2-Clause License',
+   long_description=open('README.txt').read(),
+)
+
