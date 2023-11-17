fe86881d7f13b3824496249c19960fbf55a6d87e
Michel Hardenberg <MHardenberg@proton.me>
2023-11-17 08:35:47 +0100

    finished board visualisation - now with tweaks!

--- Monopolotron/Game/Game.py
+++ Monopolotron/Game/Game.py
@@ -1,7 +1,8 @@
 from Monopolotron.Game.Player import Player
-#from Monopolotron.Game.Board import board
+# from Monopolotron.Game.Board import board
 from Monopolotron.Game import settings
 from Monopolotron.Game.draw_board import draw_board_ascii
+
 
 class Game:
     def __init__(self, players: int = 2) -> None:
@@ -13,8 +14,6 @@ class Game:
             self.players[i].name = settings.names[i]
 
         self.board_drawer = draw_board_ascii()
-        
 
     def __repr__(self,) -> str:
         return self.board_drawer.draw(self.players)
-        
