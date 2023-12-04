f97018545f658e2aeeb48c1195000e5643a2ee4d
Michel Hardenberg <MHardenberg@proton.me>
2023-12-04 22:13:00 +0100

    updated main.py

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from Monopolotron.Model.DQNAgent import DQNAgent
 from Monopolotron.Model.train_dqn import train_dqn
 import numpy as np
+import csv
 
 
 if __name__ == "__main__":
     dqn = DQNAgent()
-    hist = train_dqn(dqn, 1000)
-    chunks = 10
+    epochs = 5000
+    hist = train_dqn(dqn, epochs)
+    chunks = epochs // 500
     for chunk in np.array_split(hist, chunks):
         print(f'Success number: {np.mean(chunk)}')
+
+    with open('train_hist.csv', 'w') as f:
+        write = csv.writer(f)
+        write.writerow(hist)
